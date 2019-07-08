8143c1db2645c345056d41c25b7e9227aa433798
aarav <aarav@aarav-PC>
2019-07-08 14:16:52 -0400

    New line added

--- Java8/src/com/towerplus/java8/HelloWorld.java
+++ Java8/src/com/towerplus/java8/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Welcoem to Java 8");
 		System.out.println("New Addition to file");
+		System.out.println("addition of new lines in july");
 		
 
 	}
