c11b269237dc44be8290803b2d040c8256702c67
Admin <Admin@DESKTOP-CE9A7CR>
2019-11-16 17:56:55 +0530

    Another developer - merge demo

--- Java8/src/com/towerplus/java8/HelloWorld.java
+++ Java8/src/com/towerplus/java8/HelloWorld.java
@@ -7,8 +7,9 @@ public class HelloWorld {
 		System.out.println("New Addition to file");
 		System.out.println("addition of new lines in july");
 		System.out.println("Continuation in Nov 2019");
-		System.out.println("Happy Learning , Humble");
+		System.out.println("Happy Learning , Humble,Stay focussed");
 		System.out.println("Journey of thousand miles begins with single step");
+		System.out.println("only you can change your life");
 
 	}
 
